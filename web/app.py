09bc60a1365504b0c65287341cbb530504f931dc
Jailson Zarur <jailsoncardoz123@gmail.com>
2024-09-30 07:50:11 -0300

    Classe refill adicionada.

--- web/app.py
+++ web/app.py
@@ -124,9 +124,40 @@ class Classify(Resource):
 
         return retJson
 
+class Refill(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        admin_pw = postedData["admin_pw"]
+        amount = postedData["amount"]
+
+        if not userExist(username):
+            retJson = {
+                "status_code": 301,
+                "msg": "Username inválido."
+            }
+            return retJson
+        
+        correct_pw = "abc123"
+        if not correct_pw == admin_pw:
+            retJson = {
+                "status_code": 301,
+                "msg": "Password do admin inválido."
+            }
+            return retJson
+        
+        users.update_one({"Username": username}, {"$set": {"Tokens": amount}})
+
+        retJson = {
+            "status_code": 200,
+            "msg": "Refil feito com sucesso."
+        }
+        return retJson
     
 api.add_resource(Register, '/register')
 api.add_resource(Classify, '/classify')
+api.add_resource(Refill, '/refill')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
